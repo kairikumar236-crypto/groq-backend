37c89d68e0b6d33191558511aee9129e30d0da82
kairikumar236-crypto <kairikumar236@gmail.com>
2026-06-09 14:24:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Groq backend is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
